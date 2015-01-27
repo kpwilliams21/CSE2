131507a2ce23d1d5cb83fe99684baecfffc9157d
Korynne Williams <kpw217@lehigh.edu>
2015-01-27 03:19:56 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,34 @@
+// Korynne Williams
+// Hw 01
+// Welcome Class Java Program
+//
+//  first compile the program
+//      javac HelloWorld.java
+//  run the program
+//      java HelloWorld//
+
+// Define a class
+public class WelcomeClass {
+    
+// Add main method
+    public static void main(String [] args) {
+        
+// print the statement with box around it
+    System.out.println("   -----------");
+    System.out.println("   | WELCOME |");
+    System.out.println("   -----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^  ");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\");
+    System.out.println("<-K--P--W--2--1--7->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("I'm a sophomore in the College of Arts and Sciences. I'm majoring in Math with a minor in Actuarial Science");
+    System.out.println("test");
+    
+    //test/testtest
+        
+    }
+    
+    
+    
+}
